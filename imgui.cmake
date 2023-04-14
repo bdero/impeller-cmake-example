ef88dbfb13971f7e489a56e61515398df913ab3a
Zach Anderson <zra@google.com>
2023-04-14 07:38:59 -0700

    Add Flutter LICENSE and headers

--- imgui.cmake
+++ imgui.cmake
@@ -1,3 +1,7 @@
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 if(DEFINED FLUTTER_ENGINE_DIR)
     set(IMGUI_FLUTTER_DIR ${FLUTTER_ENGINE_DIR})
 else()
--- third_party/stb.cc
+++ third_party/stb.cc
@@ -1,2 +1,6 @@
-   #define STB_IMAGE_IMPLEMENTATION
-   #include "stb/stb_image.h"
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
