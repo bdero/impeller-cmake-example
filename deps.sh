414477a3b840afd291d71b95aa0ff064fdb0f2c5
Zach Anderson <zra@google.com>
2023-04-14 07:38:59 -0700

    Add Flutter LICENSE and headers

--- deps.sh
+++ deps.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 set -e
 
 echo "Fetching git submodules..."
--- toolchains/mac-x64-toolchain.cmake
+++ toolchains/mac-x64-toolchain.cmake
@@ -1,3 +1,7 @@
+# Copyright 2013 The Flutter Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # This toolchain definition expects the environment variable
 # FLUTTER_ENGINE_SRC_DIR to be the absolute path to a full Flutter engine
 # checkout.
